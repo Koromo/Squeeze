909e4fc18a1b88f6d022ac3a04737c3a5ea18cca
Koromo <proggr0@gmail.com>
2017-08-31 01:14:35 +0900

    Update tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,9 +1,7 @@
 #include <CppUTest/CommandLineTestRunner.h>
-#include <cstdlib>
 
 int main(int ac, char** av)
 {
     const auto code = CommandLineTestRunner::RunAllTests(ac, av);
-    std::system("pause");
     return code;
 }
